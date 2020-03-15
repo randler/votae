6a373bbdafba0078acd7cc4f1d4dc8953d5aca34
randler.ferraz <randler.ferraz@codificar.com.br>
2020-03-15 18:22:40 -0300

    alterações de votos de determinado candidato

--- voto/api/serializers.py
+++ voto/api/serializers.py
@@ -1,9 +1,12 @@
-from rest_framework.serializers import ModelSerializer, SerializerMethodField
+from rest_framework.serializers import ModelSerializer, IntegerField, SerializerMethodField, StringRelatedField
 
+from candidato.models import Candidato
 from voto.models import Voto
 
 
 class VotoSerializer(ModelSerializer):
+    def to_representation(self, instance):
+        return {'candidato': Candidato.objects.get(pk=instance['candidato']).nome, 'votos': instance['votos']}
+
     class Meta:
         model = Voto
-        fields = ['candidato']
--- voto/api/viewsets.py
+++ voto/api/viewsets.py
@@ -1,9 +1,16 @@
 from rest_framework import generics
+from django.db.models import Count
+from rest_framework.response import Response
+
 
 from voto.models import Voto
 from .serializers import VotoSerializer
 
 
 class VotoViewSet(generics.ListAPIView):
-    queryset = Voto.objects.all()
-    serializer_class = VotoSerializer
+    queryset = Voto
+
+    def list(self, request):
+        votos = Voto.objects.values('candidato').annotate(votos=Count('candidato')).order_by()
+        serializer = VotoSerializer(votos, many=True)
+        return Response(serializer.data)
